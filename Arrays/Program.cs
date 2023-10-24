de9eb411d2eb5aecb857b23c77169789c9a5b0a7
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-10-24 16:25:37 +0300

    Döngüler ögrenildi

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,26 +4,47 @@
     {
         static void Main(string[] args)
         {
-            string[] students2 = new string[3] { "Engin", "Tuana", "Efrun" };
+            //string[] students2 = new string[3] { "Engin", "Tuana", "Efrun" };
 
 
-            foreach (var student in students2)
-            {
-                Console.WriteLine(student);
-            }
-            string[,] regions = new string[7, 3]
-            {
-                { "İstanbul","İzmit","Balıkesir"},
-                { "Ankara", "Konya","Kırıkkale" },
-                { }
-                { }
-                { }
-                { }
-                { }
+            //foreach (var student in students2)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //students[0] = "Engin";
+            //students[1] = "Tuana";
+            //students[2] = "Efrun";
+            //{
+            //    Console.WriteLine(students[1]);
+            //}
 
-            }
-            Console.WriteLine();
-            Console.ReadLine();
+
+
+            //ÇOK BOYUTLU DİZELER
+            //string[,] regions = new string[5, 3]
+            //{
+            //    { "İstanbul","İzmit","Balıkesir"},
+            //    { "Ankara", "Konya","Kırıkkale" },
+            //    { "Antalya","Adana","Mersin"},
+            //    { "Rize","Trabzon","Samsun"},
+            //    { "İzmir","Mugla","Manisa"}
+
+
+            //};
+            //for (int i = 0; i < regions.GetUpperBound(0); i++)
+            //{
+            //    for (int j = 0; j < regions.GetUpperBound(1); j++) 
+            //    for (int j = 0; j < regions.GetUpperBound(1); j++) 
+            //    {
+            //        Console.WriteLine(regions[i, j]);
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+
+
+
             //students[0] = "Engin";
             //students[1] = "Tuana";
             //students[2] = "Efrun";
@@ -34,6 +55,4 @@
         }
     }
 }
-
-
 
---
+++ Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿         //FOR DÖNGÜSÜ
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Finished!");
+
+            Console.ReadLine();
+            //        }
+            //    }
+            //}
+
+
+          //WHİLE DÖNGÜSÜ
+//            int number = 100;
+//            while (number >= 0)
+//            {
+//                Console.WriteLine(number);
+//                number--;
+//            }
+//            Console.WriteLine("Now number is {0}", number);
+//            Console.ReadLine();
+//            }
+//    }
+//} 
+
+
+         //DO WHİLE DÖNGÜSÜ
+//            int number = 10;
+//            do
+//            {
+//                Console.WriteLine(number);
+//                number--;
+//            } while (number >= 0);
+//            Console.ReadLine();
+//            {
+
+
+//            }
+//        }
+//    }
+//}
+
+
+
+
+           //ForEach DÖNGÜSÜ
+//            string[] students = new string[3] { "Engin", "Tuana", "Efrun" };
+//            foreach (var student in students)
+//            {
+//                Console.WriteLine(student);
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
