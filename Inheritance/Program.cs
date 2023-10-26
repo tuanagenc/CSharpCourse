e1794f0284d35a99a9985dbb6d1477b84248ec73
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-10-26 22:32:35 +0300

    Inheritance ögrenildi.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+
+            Customer customer = new Customer();
+            Student student = new Student();
+            student.Departmant = "Software";
+
+            Person[] person = new Person[3]
+            {
+            new Customer
+            {FirstName =  "Tuana" },
+            new Student
+            {FirstName = "Yunus"},
+            new Person
+            {FirstName = "Ali"}
+            };
+            foreach (var item in person)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿
+namespace InterfacesDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+            new Managers(),
+            new Worker(),
+            new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+            new Worker(),
+            new Managers()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+ 
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Managers : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
