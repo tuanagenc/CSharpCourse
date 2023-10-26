021db32752bb167230984b6e421b899143cd421b
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2023-10-26 14:59:40 +0300

    Classes kısmı eklendi

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,27 +1,54 @@
-﻿namespace Classes
+﻿
+
+
+namespace Classes
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             CustomerManager customerManager = new CustomerManager();
             customerManager.Add();
             customerManager.Update();
-            Console.ReadLine();
-        }
 
-    }
-    class CustomerManager
-    {
-        public void Add()
-        {
-            Console.WriteLine("Customer Added!");
-        }
-        public void Update()
-        {
-            Console.WriteLine("Customer Updated!");
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+
+
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.FirstName = "Engin";
+            customer.LastName = "Demirog";
+
+            Customer customer2 = new Customer
+            {
+                Id = 1,
+                City = "İstanbul",
+
+
+            };
+            Console.WriteLine(customer.FirstName);
+            Console.ReadLine();
+
         }
     }
 }
 
+//    class CustomerManager
+//    {
+//        public void Add()
+
+//        {
+//            Console.WriteLine("Customer Added!");
+//        }
+//        public void Update()
+//        {
+//            Console.WriteLine("Customer Updated!");
+
+//        }
+//    }
+//}
 
